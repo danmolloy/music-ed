fa41f2d465ea866f65214bc565b1fedeabe18dbf
Daniel Molloy <danielmolloy_6@icloud.com>
2023-02-10 15:11:56 +0000

    bug fix

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 
 export default function Contact() {
   return (
-    <Layout contact>
+    <Layout>
       <h1>Contact</h1>
       <p>We try to respond to all messages promptly.</p>
       <p>You can also reach out to us on <a className='text-blue-500 hover:underline hover:cursor-pointer' href="https://github.com/danmolloy">Github</a>.</p>
